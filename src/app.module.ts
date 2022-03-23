dcabfcff0b5618b49a9442e45c348256ec452aff
arx <arktur.usmanov@yandex.ru>
2022-03-23 15:46:26 +0300

    added env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`
+    }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'fit-app',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_NAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       models: [],
       autoLoadModels: true,
     }),
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function start() {
-  const PORT = 8080;
+  const PORT = process.env.PORT || 8080;
   const app = await NestFactory.create(AppModule);
-  await app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
+  await app.listen(PORT, () =>
+    console.log(`Server started on port ${PORT}...`),
+  );
 }
 start();
